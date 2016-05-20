a23c7189a55997aa57ae4b15a2bfeb45ef2f8e99
Mehdi Abaakouk <sileht@sileht.net>
2016-05-20 17:36:05 +0200

    Fix tox and pep8

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,6 @@ sys.path.insert(0, os.path.abspath('../..'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     'sphinx.ext.autodoc',
-    #'sphinx.ext.intersphinx',
     'oslosphinx'
 ]
 
@@ -72,4 +71,4 @@ latex_documents = [
 ]
 
 # Example configuration for intersphinx: refer to the Python standard library.
-#intersphinx_mapping = {'http://docs.python.org/': None}
+# intersphinx_mapping = {'http://docs.python.org/': None}
