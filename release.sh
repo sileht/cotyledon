a99ed91f21b1711b8fdf5e8f6c121ff6f4cdd9b3
Mehdi Abaakouk <sileht@sileht.net>
2016-07-07 10:33:54 +0200

    release:Run py34 tests first

--- release.sh
+++ release.sh
@@ -9,7 +9,7 @@ version=$1
 status=$(git status -sz)
 [ -z "$status" ] || false
 git checkout master
-tox -epep8,py27,py34
+tox -epy34,py27,pep8
 git push 
 git tag -s $version -m "Release version ${version}"
 git checkout $version
