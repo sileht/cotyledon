3d9cf35e1f9c8418715563f7ede4bf9bc362e236
Mehdi Abaakouk <sileht@sileht.net>
2016-10-28 18:19:03 +0200

    simplify the dead worker detection/restarting

--- cotyledon/_service_manager.py
+++ cotyledon/_service_manager.py
@@ -200,10 +200,11 @@ class ServiceManager(_utils.SignalManager):
         self._wait_forever()
 
     def _on_wakeup(self):
-        dead_pid = self._get_last_pid_died()
-        while dead_pid is not None:
-            self._restart_dead_worker(dead_pid)
-            dead_pid = self._get_last_pid_died()
+        info = self._get_last_worker_died()
+        while info is not None:
+            service_id, worker_id = info
+            self._start_worker(service_id, worker_id)
+            info = self._get_last_worker_died()
         self._adjust_workers()
 
     def _on_signal_received(self, sig):
@@ -265,21 +266,12 @@ class ServiceManager(_utils.SignalManager):
                 for worker_id in range(running_workers, conf.workers):
                     self._stop_worker(service_id, worker_id)
 
-    def _restart_dead_worker(self, dead_pid):
+    def _get_last_worker_died(self):
+        """Return the last died worker information or None"""
         for service_id in self._running_services:
             # We copy the list to clean the orignal one
             processes = list(self._running_services[service_id].items())
             for process, worker_id in processes:
-                if process.pid == dead_pid:
-                    del self._running_services[service_id][process]
-                    self._start_worker(service_id, worker_id)
-                    return
-        LOG.error('pid %d not in service known pids list', dead_pid)
-
-    def _get_last_pid_died(self):
-        """Return the last died service or None"""
-        for service_id, processes in self._running_services.items():
-            for process, worker_id in processes.items():
                 if not process.is_alive():
                     if process.exitcode < 0:
                         sig = _utils.signal_to_name(process.exitcode)
@@ -288,7 +280,8 @@ class ServiceManager(_utils.SignalManager):
                     else:
                         LOG.info('Child %(pid)d exited with status %(code)d',
                                  dict(pid=process.pid, code=process.exitcode))
-                    return process.pid
+                    del self._running_services[service_id][process]
+                    return service_id, worker_id
 
     def _fast_exit(self, signo=None, frame=None,
                    reason='Caught SIGINT signal, instantaneous exiting'):
