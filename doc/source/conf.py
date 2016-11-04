ecddbd6e8c7f7f79a8bb0b4d6bdf097a0e7ff220
Mehdi Abaakouk <sileht@sileht.net>
2016-11-04 10:49:50 +0100

    Switch doc to rtd theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -56,6 +56,14 @@ pygments_style = 'sphinx'
 # html_theme = '_theme'
 # html_static_path = ['static']
 
+try:
+    import sphinx_rtd_theme
+except ImportError:
+    pass
+else:
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 # Output file base name for HTML help builder.
 htmlhelp_basename = '%sdoc' % project
 
