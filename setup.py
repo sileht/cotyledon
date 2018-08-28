c12d4af58588283ed523940b6602ea4b694df6e8
Mehdi Abaakouk <sileht@sileht.net>
2018-08-28 10:58:21 +0200

    Remove pbr from cotyledon

--- setup.py
+++ setup.py
@@ -13,17 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
 import setuptools
 
-# In python < 2.7.4, a lazy loading of package `pbr` will break
-# setuptools if some other modules registered functions in `atexit`.
-# solution from: http://bugs.python.org/issue15881#msg170215
-try:
-    import multiprocessing  # noqa
-except ImportError:
-    pass
-
 setuptools.setup(
-    setup_requires=['pbr'],
-    pbr=True)
+    setup_requires=[
+        'setuptools>=30.3.0',
+        'setuptools_scm',
+    ],
+    use_scm_version=True
+)
